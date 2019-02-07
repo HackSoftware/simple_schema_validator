5f76be1976dd22a3d223cb096df525ff0cd6c17e
Kaloyan Gerginov <kaloyan.gerginov@hacksoft.io>
2019-02-07 15:47:01 +0200

    Dot notations for addtional_keys

--- schema_validator.py
+++ schema_validator.py
@@ -1,3 +1,63 @@
+from collections import deque
+
+
+def _build_path(item, parents):
+    path = deque()
+    path.appendleft(item)
+
+    while parents[item] is not None:
+        path.appendleft(parents[item])  # [b, ]
+
+        item = parents[item]
+
+    return '.'.join(path)
+
+
+def _get_nested(d, path):
+    parts = path.split('.')
+
+    result = None
+
+    for part in parts:
+        result = d.get(part)
+
+        if result is None:
+            return None
+
+        d = d[part]
+
+    return result
+
+
+def _get_paths(d):
+    stack = deque()
+    parents = {}  # item: parent, top-level items have None as parent
+    # {
+    #     'b': None,
+    #
+    # }
+
+    for key in d:
+        # Append a tuple of (item, parent)
+        stack.append((key, None))
+        # [('a', None)
+        # ('b', none)]
+
+    while len(stack) > 0:
+        item, parent = stack.pop()
+
+        parents[item] = parent
+
+        value = _get_nested(d, _build_path(item, parents))
+
+        if type(value) is dict:
+            for key in value:
+                # Append a tuple of (item, parent)
+                stack.append((key, item))
+
+    return parents
+
+
 def _add_to_keys(keys, key, key_depth=None):
     missing_key = key
     if key_depth:
@@ -5,52 +65,6 @@ def _add_to_keys(keys, key, key_depth=None):
 
     keys.append(missing_key)
     return keys
-
-
-# def schema_validator(schema, data):
-#     def internal(schema, data, missing_keys=None, additional_keys=None, key_depth=None):
-#         used_keys = set()
-#         all_keys = set(data.keys())
-#
-#         if missing_keys is None:
-#             missing_keys = []
-#
-#         if additional_keys is None:
-#             additional_keys = []
-#
-#         if key_depth is None:
-#             key_depth = []
-#
-#         for element in schema:
-#             if type(element) is list:
-#                 key, nested_schema = element
-#                 used_keys.add(key)
-#
-#                 if key in data:
-#                     key_depth.append(key)
-#
-#                     valid, missing_keys, additional_keys = internal(
-#                         nested_schema,
-#                         data[key],
-#                         missing_keys,
-#                         additional_keys,
-#                         key_depth=key_depth
-#                     )
-#
-#                     key_depth.clear()
-#                 else:
-#                     missing_keys = _add_to_keys(missing_keys, key, key_depth)
-#             else:
-#                 used_keys.add(element)
-#
-#                 if element not in data:
-#                     missing_keys = _add_to_keys(missing_keys, element, key_depth)
-#
-#         additional_keys += [x for x in all_keys if x not in used_keys]
-#
-#         return missing_keys == [] and additional_keys == [],  missing_keys, additional_keys
-#
-#     return internal(schema, data)
 
 
 def _get_missing_keys(schema, data, missing_keys=None, key_depth=None):
@@ -89,15 +103,12 @@ def _get_missing_keys(schema, data, missing_keys=None, key_depth=None):
     return missing_keys
 
 
-def _get_additional_keys(schema, data, additional_keys=None, key_depth=None):
+def _get_additional_keys(schema, data, additional_keys=None):
     used_keys = set()
     all_keys = set(data.keys())
 
     if additional_keys is None:
         additional_keys = []
-
-    if key_depth is None:
-        key_depth = []
 
     for element in schema:
         if type(element) is list:
@@ -105,22 +116,29 @@ def _get_additional_keys(schema, data, additional_keys=None, key_depth=None):
             used_keys.add(key)
 
             if key in data:
-                key_depth.append(key)
-
                 additional_keys = _get_additional_keys(
                     nested_schema,
                     data[key],
                     additional_keys,
-                    key_depth=key_depth
                 )
 
-                # key_depth.clear()
         else:
             used_keys.add(element)
+
     additional_keys += [x for x in all_keys if x not in used_keys]
-    print(key_depth, additional_keys)
 
     return additional_keys
+
+
+def schema_validator(schema, data):
+    parents = _get_paths(data)
+
+    missing_keys = _get_missing_keys(schema, data)
+    additional_keys = _get_additional_keys(schema, data)
+
+    additional_keys = [_build_path(x, parents) for x in additional_keys]
+
+    return missing_keys == [] and additional_keys == [], missing_keys, additional_keys
 
 
 if __name__ == '__main__':
@@ -144,13 +162,15 @@ if __name__ == '__main__':
                 'd': {
                     'f': 3,
                     'e': {
-                        'g': 2
+                        'g': 2,
+                        'i': 23
                     }
                 }
             },
             'l': {
-                'a': 2
+                'a': 2,
+                'h': 34
             }
         }
 
-    print(_get_additional_keys(schema, data))
+    print(schema_validator(schema, data))
